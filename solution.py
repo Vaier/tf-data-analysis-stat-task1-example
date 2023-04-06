5ff2f628223adcf3e503ff359ac3f0c8e0ae8c2f
Vaier <62292356+Vaier@users.noreply.github.com>
2023-04-06 15:59:39 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,5 +5,4 @@ import numpy as np
 chat_id = 1251251937 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    a = np.exp(np.log(x).mean())
-    return a
+    return np.log(x-779).mean()
